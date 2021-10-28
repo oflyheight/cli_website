ea9689befc0ee7fa40b0ea5ac885539875482341
Oscar Ajquejay <87202760+oflyheight@users.noreply.github.com>
2021-10-27 20:51:36 -0600

    Add files via upload

---
+++ precache-manifest.f38228fe30fcd2832aaf2d3b7ca818c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0295ec0ec490bbbbaeca12a30eb882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d52e948b5102e3fd1fd7",
+    "url": "/static/css/main.3f8e1b76.chunk.css"
+  },
+  {
+    "revision": "1c731d268723392f6639",
+    "url": "/static/js/2.d2379dc8.chunk.js"
+  },
+  {
+    "revision": "8b8fc115f828537022995799a8358a53",
+    "url": "/static/js/2.d2379dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52e948b5102e3fd1fd7",
+    "url": "/static/js/main.c5f84a60.chunk.js"
+  },
+  {
+    "revision": "30819b5eadf223f898d0",
+    "url": "/static/js/runtime-main.af15cf85.js"
+  }
+]);
